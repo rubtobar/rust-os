9f692aeac7127499123d86191e8414d5d586d276
rubtobar <rntobar41@gmail.com>
2023-06-17 15:59:32 +0200

    Adds println functionality to the kernel

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-#![no_std]
-#![no_main]
+#![no_std] // No standard library from the OS loaded (we don't have one)
+#![no_main] // Provide our own entry point
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
 
 mod vga_buffer;
 
@@ -10,12 +12,28 @@ mod vga_buffer;
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-
+    println!("Hello World1{}", "!");
+    println!("Hello World2{}", "!");
+    println!("Hello World3{}", "!");
     loop {}
+}
+
+// Tests
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
